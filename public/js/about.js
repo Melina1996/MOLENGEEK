f88976dcdef7f79f443e0cc569dc02598db4dd5c
Melina1996 <melina.schlachter@gmx.de>
2024-01-31 14:04:22 +0100

    added ABOUT and responsive

---
+++ public/js/about.js
@@ -0,0 +1,72 @@
+import * as INSTANCE from "./modules/instances.js"
+import * as FUNCTION from "./modules/functions.js"
+import data from '../molengeek.json' assert { type: "json" }
+
+//HEADER: include logo in header
+
+FUNCTION.logoNav()
+
+//FOOTER: assign social media logos
+
+FUNCTION.socialMedia()
+
+document.querySelectorAll(".my-icon")
+      
+document.querySelectorAll(".my-icon")[0].src = "../assets/img/facebook.png"
+
+document.querySelectorAll(".my-icon")[1].src = "../assets/img/twitter.png"
+
+document.querySelectorAll(".my-icon")[2].src = "../assets/img/instagram.png"
+
+document.querySelectorAll(".my-icon")[3].src = "../assets/img/linkedin.png"
+
+//FOOTER Logo Molengeek
+
+FUNCTION.logoFooter()
+
+//NAVBAR WITH DROPDOWN
+
+FUNCTION.dropDown()
+
+//BURGER-MENU: on click all my links appear
+
+FUNCTION.myBurger()
+
+//SCROLL-BTN
+
+//Button on scroll//
+
+//creation of variable for my button (called with its id)//
+
+window.onscroll = function() {FUNCTION.scrollFunction()};
+
+
+//when the user clicks on it, the scroll goes back to top of the page//
+
+INSTANCE.myScrollbutton.addEventListener("click",()=>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
+
+//BANNER: change bckg-color & first section: change colors
+
+let myBanner = document.querySelector(".banner-slogan")
+
+myBanner.style.background = "#984d89"
+
+let myWhoWeAre = document.querySelector(".who-we-are")
+
+myWhoWeAre.style.background = "white"
+
+let myH1WhoWeAre = document.querySelector(".text-left").querySelector("h1")
+
+myH1WhoWeAre.style.color = "black"
+
+//Text style changes
+
+let myTextRight = document.querySelector(".text-right").querySelector("h1")
+
+myTextRight.querySelectorAll("span").forEach(element => {
+    element.style.color = "#c8b747"
+    element.style.fontWeight = "bolder"
+});
